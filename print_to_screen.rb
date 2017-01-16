327fb40af6b4fe70eca5bbe80a4a8f40b6083341
Kevin Glass <Kevin Glass>
2017-01-16 14:23:48 -0500

    change 5 to minds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,11 +1,12 @@
 number = 1
 
 100.times do 
-
-	if number == 3
-		puts 'mined'
-		number = number +1 
-	else 
-		puts number = number + 1	
+	if number % 3 == 0
+		puts 'mined'		
+	elsif number ==5 
+		puts 'minds'
+		else 
+		puts number 	
 	end	
+	number = number + 1 
 end
