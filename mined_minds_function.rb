ff2f42262b6ac14dde805f9fc2941c7988b13103
Kevin Glass <Kevin Glass>
2017-01-16 17:12:06 -0500

    changing 3 to mined

--- mined_minds_function.rb
+++ mined_minds_function.rb
@@ -1,3 +1,8 @@
 def get_mined_minds_results(number)
 	number
+	if number == 3
+		'mined'
+	else
+		number
+end
 end
--- test_mined_mind_fuctions.rb
+++ test_mined_mind_fuctions.rb
@@ -10,4 +10,8 @@ class TestMMFunctions < Minitest::Test
 	def test_two_returns_two() 
 		assert_equal(2, get_mined_minds_results(2))
 	end
+	def test_three_returns_mined()
+		assert_equal('mined', get_mined_minds_results(3))
+	end
+
 end
