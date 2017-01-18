6b1a6564d1ab87ce90e5d5c7807df9c5f5d89ed2
Kevin Glass <Kevin Glass>
2017-01-17 19:19:05 -0500

    exercise 5

---
+++ lrthw5.rb
@@ -0,0 +1,17 @@
+my_name = 'kevin glass'
+my_age = 35 # not a lie last year
+my_height = 71 # inches
+my_weight = 220 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
