4f1095397513e9c690cf53f4ccfc5e0dede9ab72
Kevin Glass <Kevin Glass>
2017-01-19 10:37:02 -0500

    pass 9 to mineds

--- text_functions.rb
+++ text_functions.rb
@@ -10,12 +10,16 @@ class TestMMFunctions < Minitest::Test
 	def test_one_to_mined_minds_functionreturns_1()
 		assert_equal(2, mined_minds_function(2))
 	end
-	
+
 	def test_two_to_mined_minds_function_returns_2()
 		assert_equal(2, mined_minds_function(2))
 	end
 	
-	def test_three_to_mind_minds_function_returns_2
+	def test_three_to_mined_minds_function_returns_2
 		assert_equal('mined', mined_minds_function(3))
+	end
+
+	def test_nine_to_mined_minds_function_returns_2
+		assert_equal('mined', mined_minds_function(9))
 	end
 end
