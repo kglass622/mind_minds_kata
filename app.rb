9916d6a03d6907b579273363f7c240c67de6ae4c
Kevin Glass <Kevin Glass>
2017-02-13 16:30:20 -0500

    playing with erb

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require_relative 'isbn_in_class.rb'
+
+get '/' do
+#	'hello homie'
+erb :isbn_input, :locals => { :results => '', :isbn_num => '', :message_one => '', :number => ''}
+end
+
+post '/isbn_num' do
+	isbn_num = params[:isbn_input]
+	# "#{isbn_input}"
+
+results = valid_isbn?(isbn_num)
+	# "#{results} results found here"
+erb :isbn_input, :locals => {:isbn_num => isbn_num, :results => results, :message_one => ' is a', :number => ' ISBN'}
+#	"#{results}"
+
+end
+
