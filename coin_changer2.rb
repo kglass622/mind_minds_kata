0753eced0af6e39b99de67609c7fcb1fa19059f1
Kevin Glass <Kevin Glass>
2017-02-15 22:25:41 -0500

    test for 3,4 pennies and nickel

--- coin_changer2.rb
+++ coin_changer2.rb
@@ -1,14 +1,31 @@
+# Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
+# eg. If you have 11 cents you should return {:dime => 1, :penny => 1};
+# eg. If you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1};
+# eg. If you have 93 cents {:quarter => 3, :dime => 1, nickel => 1, :penny => 3}
+# Start a new repo
+# Build up your coin_changer using tests. Commit to git after every test.
+
+
 def coin_changer(change)
 	broke = {}
 	dime = 10
 	quarter = 25
+	penny = 1
+	nickel = 5
 
 	if change == 0
 		return broke
 	end
 
+	if change >= 5
+		broke[:nickel] = 1
+	#	change = change - 5
+	end
+
 	if change >= 1
 		broke[:penny] = change
 	end
+	
+	
 	broke
 end
--- test_coin_changer2.rb
+++ test_coin_changer2.rb
@@ -14,4 +14,16 @@ class Test_coin_changer2 < Minitest::Test
 	def test_for_2_pennies
 		assert_equal({:penny => 2}, coin_changer(2))
 	end
+
+	def test_for_3_pennies
+		assert_equal({:penny => 3}, coin_changer(3))
+	end
+
+	def test_for_4_pennies
+		assert_equal({:penny => 4}, coin_changer(4))
+	end
+
+	def test_1_nickel
+		assert_equal({:nickel => 1}, coin_changer(5))
+	end
 end
